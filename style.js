d5118f792e7ea026ff12cb75ca56f66402efe9a5
DidoFayed <80528848+DidoFayed@users.noreply.github.com>
2021-11-18 01:27:43 +0700

    updatee

---
+++ style.js
@@ -0,0 +1,10 @@
+$("#slideshow-container > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow-container');
+}, 3000);
